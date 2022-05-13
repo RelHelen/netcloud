f27eefc9408610586e701dbd034635ddef07277b
RelHelen <relmich2@gmail.com>
2022-05-13 20:18:16 +0300

    select contracts

--- app/controllers/ContractsController.php
+++ app/controllers/ContractsController.php
@@ -12,28 +12,54 @@ use app\models\User;
 class ContractsController extends AppController
 {
   public $user;
-
+  public $model;
+  public $contracts;
   public function __construct($route)
   {
     parent::__construct($route); //сначало выполняем
 
     //проверка переменной из сессии при авторизации    
     if (!$this->isUserLog($this->route['action'], $this->route['controller'])) {
-      //debug($_SESSION['user']);
-
       redirect(PATH . '/user/login');
+    } else {
+      $this->model = new Contracts; //модель Контрактов
+      $this->contracts = $this->model->getContractsAll(); //получили договора
+
+      // debug($_SESSION['user']);
     }
-    // debug($_SESSION['user']);
   }
 
 
-
+  /**
+   * вывод всех договоров
+   */
   public function indexAction()
   {
 
     $this->setTitle('Договора'); //установка заголовка
-    $model = new Contracts; //модель Контрактов
-    $model->getContractsAll(); //получили договора
-    //debug($_SESSION['user']['users_login']);
+
+    // $contracts = $this->model->getContractsAll(); //получили договора
+    // $this->contracts = $contracts;
+
+    if ($this->contracts) {
+      // debug($this->contracts);
+      $contracts = $this->contracts;
+      // die;
+      $this->setData(compact('contracts'));
+    };
+  }
+
+  /**
+   * вывод конкретного договора
+   */
+  public function viewAction()
+  {
+    $alias = $this->route['alias'];
+    $contract = $this->model->getContract($alias);
+    $devices = $this->model->getDevicesAll($contract['id']);
+    //debug($contract);
+    //debug($devices);
+    //die;
+    $this->setData(compact('contract', 'devices'));
   }
 }
